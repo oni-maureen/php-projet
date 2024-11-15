0ddc4ca02f80b9310fa335098eaeae8f7aa92a34
Amin <amin.eddine31@icloud.com>
2024-11-15 15:19:11 +0100

    second comm Amin

--- connexion.php
+++ connexion.php
@@ -4,13 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-<form method="POST" action="connexion.php">
-        <label for="nom">Nom :</label>
-        <input type="text" id="id" name="nom" required><br>
-        <button type="submit">Se connecter</button>
+<form method="POST" action="connexion.php" style="margin: 10px 10px 10px 10px;">
+        <label for="nom"  placeholder="Username">Nom :</label>
+        <input type="text" id="id" name="nom"  placeholder = "Entrez votre nom " ><br>
+        <button type="submit" class="btn btn-info " style="margin-top: 10px;">Se connecter</button>
     </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
 
