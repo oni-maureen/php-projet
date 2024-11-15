b53c6fb401ae36e3cb0d4deda6a5c9dbd3a53ed6
Inès <120586445+Michette@users.noreply.github.com>
2024-11-15 15:49:46 +0100

    page soumettre des idées terminée

--- soumettre-idee.php
+++ soumettre-idee.php
@@ -3,10 +3,46 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Soumettre des idées</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid justify-content-end">
+            <ul class="nav">
+                <li class="nav-item">
+                    <a href="idees.php" class="nav-link">Voir les idées</a>
+                </li>
+                <li class="nav-item">
+                    <a href="deconnexion.php" class="nav-link">Déconnexion</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container mt-4">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-10 col-lg-8">
+                <div class="card">
+                    <div class="card-header">
+                        <p class="h4">Soumettre une nouvelle idée</p>
+                    </div>
+                    <form method="POST" action="soumettre-idee.php">
+                        <div class="m-4">
+                            <label for="titre" class="form-label mt-3">Titre :</label><br>
+                            <input type="text" id="titre" name="titre" class="form-control" required>
+                        </div>
+                        <div class="m-4">
+                            <label for="description" class="form-label">Description :</label><br>
+                            <textarea id="description" name="description" rows="4" class="form-control"></textarea>
+                        </div>
+                        <div class="text-center mb-4">
+                            <button type="submit" class="btn btn-info">Soumettre l'idée</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
 
@@ -16,4 +52,32 @@
 // Soumettre le formulaire en POST 
 // Ajouter l'idée dans le JSON.
 // Bouton Deconnection, Retour vers idées.
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $titre = htmlspecialchars($_POST['titre']);
+    $description = htmlspecialchars($_POST['description']);
+    $auteur = htmlspecialchars($_SESSION['login']);
+    $dateDeCreation = date('d/m/Y');
+    $fichierJson = './idees.json';
+
+    if (file_exists($fichierJson)) {
+        $contenu = file_get_contents($fichierJson);
+        $data = json_decode($contenu, true); 
+    } else {
+        $data = []; // Si le fichier n'existe pas, on initialise un tableau vide
+    }
+
+    $nouvelleIdee = [
+        "id_idee" => uniqid(), // Générer un ID unique
+        "titre" =>  $titre,
+        "description" => $description,
+        "auteur" => $auteur,
+        "dateDeCreation" => $dateDeCreation 
+    ];
+
+    $data[] = $nouvelleIdee;
+    $jsonMisAJour = json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+    file_put_contents($fichierJson, $jsonMisAJour);
+
+}
 ?>
